71fa5d0867a369cd23529d4c36c6738300abc2cf
Ivan Andjiev <ivan.andjiev@hotmail.com>
2020-02-05 23:05:23 +0100

    Init

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../sidebar/sidebar.css';
-import { NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 import logo from '../../assets/logo-white.png';
@@ -13,32 +13,31 @@ const Sidebar = () => {
             onSelect={(selected) => {
             }}
         >
-
             <SideNav.Toggle />
             <SideNav.Nav defaultSelected="home">
                 <NavItem eventKey="home">
                     <NavIcon>
-                    <NavLink to="/"><i className="fa fa-search" style={{ fontSize: '1.5em' }} /></NavLink>
+                        <NavLink to="/"><i className="fa fa-search" style={{ fontSize: '1.5em' }} /></NavLink>
                     </NavIcon>
                     <NavText>
-                    <NavLink to="/">Explore</NavLink>
-                </NavText>
+                        <NavLink to="/">Explore</NavLink>
+                    </NavText>
                 </NavItem>
                 <NavItem eventKey="favourites">
                     <NavIcon>
-                    <NavLink to="/favourites"><i className="fa fa-star-o" style={{ fontSize: '1.5em' }} /></NavLink>
+                        <NavLink to="/favourites"><i className="fa fa-star-o" style={{ fontSize: '1.5em' }} /></NavLink>
                     </NavIcon>
                     <NavText>
-                       <NavLink to="/favourites">Favourites</NavLink>
-                </NavText>
+                        <NavLink to="/favourites">Favourites</NavLink>
+                    </NavText>
                 </NavItem>
                 <NavItem eventKey="popular">
                     <NavIcon>
-                    <NavLink to="/popular"><i className="fa fa-fire" style={{ fontSize: '1.5em' }} /></NavLink>
+                        <NavLink to="/popular"><i className="fa fa-fire" style={{ fontSize: '1.5em' }} /></NavLink>
                     </NavIcon>
                     <NavText>
-                    <NavLink to="/popular">Popular</NavLink>
-                </NavText>
+                        <NavLink to="/popular">Popular</NavLink>
+                    </NavText>
                 </NavItem>
                 <img src={logo} className="logo" alt="Logo" />
             </SideNav.Nav>
