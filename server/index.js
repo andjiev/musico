e6f70e9759a3a888bed31aab5c01c7dee786a530
Ivan Andjiev <ivan.andjiev@hotmail.com>
2020-10-05 02:52:12 +0200

    babel and scripts update

--- server/index.js
+++ server/index.js
@@ -2,28 +2,4 @@ require('dotenv').config({
   path: `.env${process.env.NODE_ENV ? '.' + process.env.NODE_ENV : ''}`,
 });
 
-import express from 'express';
-import cors from 'cors';
-import { ApolloServer } from 'apollo-server-express';
-
-import typeDefs from './schema/typeDefs';
-import resolvers from './schema/resolvers';
-import spotifyApi from './datasources/spotify';
-
-const app = express();
-app.use(cors({ origin: process.env.CLIENT_APP_URL }));
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => ({
-    spotifyApi,
-  }),
-});
-
-server.applyMiddleware({ app });
-
-const port = process.env.GRAPHQL_APP_PORT || 4000;
-app.listen({ port }, () => {
-  console.log(`Server started at ${port}`);
-});
+module.exports = require('./server');
---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+
+import typeDefs from './schema/typeDefs';
+import resolvers from './schema/resolvers';
+import spotifyApi from './datasources/spotify';
+
+const app = express();
+app.use(cors({ origin: process.env.CLIENT_APP_URL }));
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    spotifyApi,
+  }),
+});
+
+server.applyMiddleware({ app });
+
+const port = process.env.GRAPHQL_APP_PORT || 4000;
+app.listen({ port }, () => {
+  console.log(`Server started at ${port}`);
+});
